de60d14cda6466fab5f96f5cbc96ece9db0aae84
enzotorrs <enzotorres969@gmail.com>
2024-02-22 19:11:19 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-main id="main">
+      <SideMenu />
+      <Header />
+      <GridAssets />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+//
+</script>
+
+<style>
+:root {
+  --primary-bg-color: #0F0F14;
+  --secondary-bg-color: #15151C;
+  --primary-accent-color: #6661FF;
+  --secondary-accent-color: #E04C63;
+  --primary-contrast-color: #20222B;
+}
+
+* {
+  margin: 0;
+  outline: none;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+#main {
+  display: grid;
+  grid-template-areas:
+    "aside header"
+    "aside main";
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto 1fr;
+  min-height: 100vh;
+}
+
+GridAssets {
+  grid-area: main;
+}
+
+Header {
+  grid-area: header
+}
+
+SideMenu {
+  grid-area: aside;
+}
+</style>
