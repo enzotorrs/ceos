0b3d0bc71a839570435848ed1787dde21da3cabf
enzotorrs <enzotorres969@gmail.com>
2024-02-24 08:56:06 -0300

    feat: add Asset component

---
+++ src/components/Asset.vue
@@ -0,0 +1,116 @@
+<template>
+  <div v-if="!deleted" class="asset" :class="{ folder: asset.folder, file: !asset.folder, disabled: disabled }"
+    @mouseover="hover = true" @mouseleave="hover = false">
+    <div class="asset__footer">
+      <div class="asset__metadata_wrapper">
+        <p class="asset__name">{{ asset?.name }}</p>
+        <p v-if="asset.folder" class="asset__count_child">{{ asset.child_assets.length }} items</p>
+        <p v-if="!asset.folder" class="asset__count_child">{{ created_date }}</p>
+      </div>
+      <button :class="{ disabled: !hover }" class="asset__tool_button">
+        <v-icon :icon="mdiDotsVertical" color="grey" />
+        <v-menu activator="parent">
+          <v-list>
+            <v-list-item v-for="(option, index) in options" :key="index" :value="index" @click="option.action">
+              <v-list-item-title>{{ option.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Asset } from '@/Asset'
+import { PropType } from 'vue'
+import axios from 'axios';
+import { mdiDotsVertical } from '@mdi/js';
+import { ref } from 'vue'
+
+const hover = ref(false)
+const deleted = ref(false)
+const disabled = ref(false)
+
+const options = [{ title: "Delete", id: 0, action: deleteAsset }]
+
+const props = defineProps({
+  asset: {
+    type: Object as PropType<Asset>,
+    required: true
+  }
+})
+
+const created_date = new Date(props.asset.created_at).toDateString()
+function deleteAsset() {
+  axios
+    .delete(`http://localhost:8000/assets/${props.asset?.id}`)
+    .then(response => console.log(response.data))
+
+  disabled.value = true
+  setTimeout(() => {
+    deleted.value = true
+  }, 400)
+}
+</script>
+
+<style>
+.folder {
+  background-image: url("@/assets/folder.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.file {
+  background-color: #303247;
+  border-radius: 8px;
+}
+
+.asset {
+  position: relative;
+  aspect-ratio: calc(170/144);
+  padding: 12px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  transition: visibility 1s linear, opacity 0.3s linear;
+}
+
+.asset__footer {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.asset__metadata_wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  overflow-x: hidden;
+
+}
+
+.asset__name {
+  color: #fff;
+  font-size: 16px;
+  overflow: hidden;
+}
+
+.asset__count_child {
+  color: #888;
+  font-size: 10px;
+}
+
+.asset__tool_button {
+  position: absolute;
+  right: 0;
+  bottom: 10px;
+  transition: visibility 1s linear, opacity 0.3s linear;
+}
+
+.disabled {
+  visibility: hidden;
+  opacity: 0;
+}
+</style>
