cdf05e0c387ca84c49ff1c2494fa0cc7962dbc70
enzotorrs <enzotorres969@gmail.com>
2024-02-25 17:52:09 -0300

    feat: add pinia to project, for state managing

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,20 @@
  */
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
 
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
 
-registerPlugins(app)
+registerPlugins(app);
 
-app.mount('#app')
+app.use(pinia);
+app.mount("#app");
