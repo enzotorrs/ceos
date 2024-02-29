2c9e174f7856b36cecf42e90b33191c8dc6bbdd6
enzotorrs <enzotorres969@gmail.com>
2024-02-29 19:59:15 -0300

    feat: add rename method on asset store

--- src/stores/assets.ts
+++ src/stores/assets.ts
@@ -12,8 +12,17 @@ export const useAssets = defineStore("assets", {
         .get("http://localhost:8000/assets")
         .then(response => this.assets = response.data);
     },
-    async deleteAsset(id: number){
-      await axios.delete(`http://localhost:8000/assets/${id}`)
+    async deleteAsset(id: number) {
+      await axios.delete(`http://localhost:8000/assets/${id}`);
+    },
+    renameAsset(id: number, newName: string) {
+      const asset = this.assets.find(asset => asset.id === id);
+      asset.name = newName;
+      axios.put(`http://localhost:8000/assets/${id}`, asset).then(response => {
+        const index = this.assets.findIndex(asset => asset.id === id);
+        this.assets[index] = response.data;
+      });
     }
+
   }
 });
