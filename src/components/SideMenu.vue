bab7526d26928bc8da0f75f10d3262ef642b52af
enzotorrs <enzotorres969@gmail.com>
2025-02-17 15:36:08 -0300

    feat: add hambuguer to sideMenu

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,64 +1,21 @@
 <template>
   <aside class="side-menu">
     <div class="logo-wrapper">
-      <img
-        src="@/assets/planet_logo.png"
-        alt="ceos logo planet"
-      >
-      <img
-        src="@/assets/text_logo.svg"
-        alt="ceos logo text"
-      >
+      <v-btn class="hamburguer-menu" :icon="mdiMenu" variant="text"/>
+      <Logo/>
     </div>
-    <navbar>
-      <ul>
-        <li
-          v-for="(item, index) in items"
-          :key="index"
-          class="navbar__item"
-          :class="{ navbar__item_active: activeIndex === index }"
-          @click="setActiveIndex(index)"
-        >
-          {{ item }}
-        </li>
-      </ul>
-      <hr class="separator">
-      <ul>
-        <li
-          class="navbar__item"
-          :class="{ navbar__item_active: activeIndex === 4 }"
-          @click="setActiveIndex(4)"
-        >
-          History
-        </li>
-      </ul>
-    </navbar>
   </aside>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { defineComponent } from "vue";
-export default defineComponent({
-    name: "SideMenu",
-    data () {
-        return {
-            activeIndex: 0,
-            items: ["Home", "Videos", "Photos"]
-        };
-    },
-    methods: {
-        setActiveIndex (index: number) {
-            this.activeIndex = index;
-        }
-    }
-});
-
+import { mdiMenu } from '@mdi/js';
 </script>
 
-<style>
+<style scoped>
 .side-menu {
   background-color: var(--primary-bg-color);
-  width: 240px;
+  width: 260px;
   padding: 22px 16px;
   display: flex;
   flex-direction: column;
@@ -66,37 +23,12 @@ export default defineComponent({
   grid-area: aside
 }
 
+.hamburguer-menu {
+  position: absolute;
+  left: 3px;
+}
+
 .logo-wrapper {
-  height: 30px;
-  display: flex;
-  align-items: center;
-  gap: 9px;
-  width: 100%;
-  justify-content: center;
   margin-bottom: 65px;
-}
-
-img {
-  height: 100%;
-}
-
-.navbar__item {
-  height: 40px;
-  width: 200px;
-  padding: 10px 16px;
-  display: flex;
-  align-items: center;
-  color: #fff;
-  border-radius: 8px;
-}
-
-.navbar__item_active {
-  background-color: var(--primary-contrast-color);
-}
-
-.separator {
-  border-top: 1px solid #fff;
-  border-bottom: none;
-  margin: 8px 0px;
 }
 </style>
