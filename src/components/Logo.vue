9d1ea6004c73ffc277bb2caa5e3757ae1df8b2b7
enzotorrs <enzotorres969@gmail.com>
2025-02-17 15:35:53 -0300

    feat: add component to logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-link to="/">
+    <div class="logo-wrapper">
+      <img src="@/assets/planet_logo.png" alt="ceos logo planet">
+      <img src="@/assets/text_logo.svg" alt="ceos logo text">
+    </div>
+  </router-link>
+</template>
+
+<style>
+.logo-wrapper {
+  height: 30px;
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  width: 100%;
+  justify-content: center;
+}
+
+img {
+  height: 100%;
+}
+</style>
