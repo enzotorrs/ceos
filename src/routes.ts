09b096ed74094f1e39ea5f3dac3fc79d3f9964e5
enzotorrs <enzotorres969@gmail.com>
2025-02-17 10:47:14 -0300

    feat: add assetView and Not found page routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from './view/HomePage.vue'
+import AssetView from './view/AssetView.vue'
+import NotFound from './view/NotFound.vue'
 
 const routes = [
+  { path: '/asset/:assetId', component: AssetView },
   { path: '/', component: HomePage },
+{ path: '/:pathMatch(.*)*', component: NotFound },
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
